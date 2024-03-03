7e575b97b5e8b1ce663e7d4ba08c88afbcaa8016
dishil29 <zalavadiyadishil@kgpian.iitkgp.ac.in>
2024-03-03 17:54:13 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+
+@login_required(login_url= '/login/')
+def home(request):
+
+  return render(request , 'home.html')
+
+def login_page(request):
+  if request.method == "POST":
+    data = request.POST
+    username = data.get("email")
+    password  = data.get('password')
+
+    if User.objects.filter(username = username).exists():
+      user = authenticate(username = username, password = password)
+      if user is None:
+        messages.error(request, 'Invalid password')
+        return redirect('/login/')
+      else:
+        login(request , user)
+        return redirect('/home/')
+    else :
+      messages.error(request, 'Invalid username')
+      return redirect('/login/')
+  return render(request, 'login.html')
+
+def register(request):
+  if request.method == "POST":
+    data = request.POST
+    email = data.get('email')
+    name = data.get("name")
+    password = data.get("password")
+
+    user = User.objects.filter(username = email)
+    if user.exists():
+      messages.info(request, "Email already registered")
+      return redirect('/register/')
+
+    user = User.objects.create( username=email, first_name = name )
+    user.set_password(password)
+    user.save()
+
+    messages.info(request, "Account created successfully")
+    
+    return redirect('/register/')
+  return render(request, 'register.html')
